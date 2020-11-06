780a25abfc46fccfdcb2dcf842aac4e655bda4e4
HERNAN JAVIER <hernanhjmq12345@gmail.com>
2020-11-06 16:06:28 +0100

    version final 2

--- librerias/conexionBD.php
+++ librerias/conexionBD.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'sql309.epizy.com';
-$user = 'epiz_26705612';
-$pass = '7GaGj8SrobJPyCs';
-$bdat = 'epiz_26705612_bd_blog';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$bdat = 'bd_blog';
 $conn = new mysqli($host, $user, $pass, $bdat);
 if($conn->connect_errno > 0)
 	{
